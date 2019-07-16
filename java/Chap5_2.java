fbba40bba2ec964098901bcc6d7ac5a32c2d829b
eunsil <eunsil1023@gmail.com>
2019-07-16 21:37:15 +0900

    /master/조건 연산자

---
+++ java/Chap5_2.java
@@ -0,0 +1,17 @@
+public class Chap5_2 {
+    public static void main(String[] args) {
+
+        int num1 = 50;
+        int num2 = 100;
+
+        if (num1 > num2) {
+            System.out.println("큰 수 :" + num1);
+        } else
+            System.out.println("큰 수 :" + num2);
+
+        if (num1 > num2) {
+            System.out.println("절댓값 :" + (num1 - num2));
+        } else
+            System.out.println("절댓값 :" + (num2 - num1));
+    }
+}
