efcb156f2b720a052e59ef8db3876cc8d98d247b
eunsil <eunsil1023@gmail.com>
2019-07-16 22:41:46 +0900

    /master/for문의 활용_2

---
+++ java/Chap5_5_2.java
@@ -0,0 +1,9 @@
+public class Chap5_5_2 {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int sum = 1; sum <= 9; sum++) {
+            System.out.println(num + " X " + sum + " = " + (num * sum));
+        }
+    }
+}
