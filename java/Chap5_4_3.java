dd4d1209b3b730468543e36273d308114da6d6cc
eunsil <eunsil1023@gmail.com>
2019-07-16 22:41:19 +0900

    /master/while문과 do ~ while문의 활용_1

---
+++ java/Chap5_4_3.java
@@ -0,0 +1,15 @@
+public class Chap5_4_3 {
+    public static void main(String[] args) {
+        int num = 1;
+        int sum = 0;
+
+        while (num <= 1000) {
+            if (((num % 2) == 0) && ((num % 7) == 0)) {
+                System.out.println(num);
+                sum += num;
+            }
+            num++;
+        }
+        System.out.println("2의 배수이자 7의 배수인 수들의 합: " + sum);
+    }
+}
