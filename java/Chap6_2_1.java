c935bfea8dae1ad65bbaef66d2aeb0fe43aeba3e
eunsil <eunsil1023@gmail.com>
2019-07-23 04:25:21 +0900

    master/값을 반환하는 메소드의 정의_1

--- java/Chap6_2_1.java
+++ java/Chap6_2_1.java
@@ -2,6 +2,7 @@
 인자로 원의 반지름 정보를 전달하면, 원의 넓이를 계산해서 반환하는 메소드와 원의 둘레를 계산해서 반환하는 메소드를 각각 정의하고,
 이 둘을 호출하는 main 메소드를 정의하자.
  */
+
 public class Chap6_2_1 {
     public static void main(String[] args) {
         double PI = 3.14;    // 원주율
