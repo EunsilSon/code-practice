3e5d8c6c8b620eb185b27aef704fda459403ee3a
eunsil <eunsil1023@gmail.com>
2019-07-23 04:20:13 +0900

    master/continue문과 break문의 활용_1

--- java/Chap6_1_1.java
+++ java/Chap6_1_1.java
@@ -1,3 +1,7 @@
+/*
+정수 둘을 인자로 전달받아서 두 수의 사칙연산 결과를 출력하는 메소드와 이 메소드를 호출하는 main 메소드를 정의해 보자.
+단 나눗셈은 몫과 나머지를 각각 출력해야 한다.
+ */
 public class Chap6_1_1 {
     public static void main(String[] args) {
         calculate(5,10);
