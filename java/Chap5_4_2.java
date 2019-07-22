05e70ba3becea5b021f0882cf5bb62bc6880844b
eunsil <eunsil1023@gmail.com>
2019-07-23 04:19:21 +0900

    master/while문과 do ~ while문의 활용_2

--- java/Chap5_4_2.java
+++ java/Chap5_4_2.java
@@ -1,3 +1,7 @@
+/*
+1부터 100까지 출력하고 이어서 거꾸로 100에서 1까지 출력하는 프로그램을 작성해보자.
+단 가급적 while문과 do ~ while문을 한 번씩 사용해서 구현하자.
+ */
 public class Chap5_4_2 {
     public static void main(String[] args) {
         int num = 1;
