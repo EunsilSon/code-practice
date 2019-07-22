9879ae68f40e6483c9e7211950ca8402522ace3f
eunsil <eunsil1023@gmail.com>
2019-07-23 04:19:29 +0900

    master/while문과 do ~ while문의 활용_3

--- java/Chap5_4_3.java
+++ java/Chap5_4_3.java
@@ -1,3 +1,6 @@
+/*
+1000 이하 자연수 중에서 2의 배수이자 7의 배수인 수를 출력하고, 그 수들의 합을 구해서 출력하는 프로그램을 while문을 이용해서 작성해보자,
+ */
 public class Chap5_4_3 {
     public static void main(String[] args) {
         int sum = 0;
