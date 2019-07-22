d90c248b200b1643777c6f7bd09033440e999970
eunsil <eunsil1023@gmail.com>
2019-07-22 23:26:26 +0900

    master/while문과 do ~ while문의 활용_3

--- java/Chap5_4_3.java
+++ java/Chap5_4_3.java
@@ -1,7 +1,7 @@
 public class Chap5_4_3 {
     public static void main(String[] args) {
-        int num = 1;
         int sum = 0;
+        int num = 1;
 
         while (num <= 1000) {
             if (((num % 2) == 0) && ((num % 7) == 0)) {
