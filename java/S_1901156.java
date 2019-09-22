7274c3192a21b1c1ef958c38e0a3e4efcacc3bb2
손은실 <eunsil1023@gmail.com>
2019-09-22 22:47:55 +0900

    master-task/#3/형 변환 연습

---
+++ java/S_1901156.java
@@ -0,0 +1,18 @@
+public class S_1901156 {
+	public static void main(String[] args) {
+		
+		float a = (float) 8.9;
+		float b = (float) 4.2;
+		
+		System.out.println("a�� b�� ���� ������ �� = " + (a % b));
+		System.out.println("a + b�� �� ���������� ��ȯ = " + (int)(a + b));
+		System.out.println("a�� b�� ���� ��ȯ �� + ���� �� �� = " + ((int)a + (int)b));
+		System.out.println("����ǥ 5�� ��� " + "\"" + "\"" + "\"" + "\"" + "\"");
+		System.out.println("�齽���� 5�� ��� " + "\\" + "\\" + "\\" + "\\" + "\\");
+		System.out.println("a < 0 ��� ��� : " + (a < 0));
+		System.out.println("b > 3 ��� ��� : " + (b > 3));
+		
+		
+	}
+
+}
