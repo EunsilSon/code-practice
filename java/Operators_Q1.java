d51b63e2690c7d5dea5388c18c500d246ae4aff4
soneunsil <eunsil1023@gmail.com>
2020-02-16 19:33:17 +0900

    master/Java 세미나 문제/연산자_1

--- java/Operators_Q1.java
+++ java/Operators_Q1.java
@@ -1,11 +1,12 @@
-public class Hw1_Q3 {
+public class Operators_Q1 {
     public static void main(String args[]) {
         /*
-        4. 상점에 가면 우리는 상품에 대한 돈을 내고 영수증을 받는다.
+        상점에 가면 우리는 상품에 대한 돈을 내고 영수증을 받는다.
         영수증에서는 10% 부가세와 잔돈 등이 나온다고 하자.
         구입한 상품의 가격과 손님한테 받은 금액을 정하면 부가세와 잔돈을 출력하는 코드를 작성해 보세요.
         ex) 받은 돈 10000 상품의 가격 7500 이라고하면 -> "부가세: 750 잔돈: 2500" 출력
         */
+
         int product = 5000;
         int money = 8000;
         int surtax = product / 10;
