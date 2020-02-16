25b74e4c6fb9aa8606439ec9daaa2a4b40c9e091
soneunsil <eunsil1023@gmail.com>
2020-02-16 19:35:21 +0900

    master/Java 세미나 문제/연산자_5

--- java/Operators_Q5.java
+++ java/Operators_Q5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Hw2_Q3 {
+public class Operators_Q5 {
     public static void main(String[] args) {
     /*
         m(미터)를 입력 받아 ft(피트)로 변환하는 프로그램을 작성하시오. ( 1ft = 0.304m )
