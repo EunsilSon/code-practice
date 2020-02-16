e3c3d995b3f1bf9547646ccf1a15a387416439c3
soneunsil <eunsil1023@gmail.com>
2020-02-16 19:28:17 +0900

    master/Java 세미나 문제/변수_1

---
+++ java/Variable_Q1.java
@@ -0,0 +1,13 @@
+public class Variable_Q1 {
+    public static void main(String args[]) {
+        /*
+        정수형 변수 A, B, C 를 만들어 A 와 B 에 정수 값을 주고 A 와 B 를 더한 값을 C 에 준 후 A, B, C 변수를 하나의 출력문에 사용해 "[A의값] + [B의값] = [C의 값]" 을 출력하라.
+        */
+
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
