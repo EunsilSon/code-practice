231327787e01f6fe951f67891077c3e045fef16e
soneunsil <eunsil1023@gmail.com>
2020-02-18 17:28:13 +0900

    master-task/#25/연산자의 활용과 연산의 특성 파악_3

---
+++ java/윤성우의 열혈 Java 프로그래밍/Chap4_1_3.java
@@ -0,0 +1,11 @@
+/*
+식 {(25 x 5) + (36 - 4) - 72} / 5의 계산 결과를 출력하는 프로그램을 작성해보자.
+*/
+public class Chap4_1_3 {
+    public static void main(String[] args) {
+        int result;
+        result = ((25 * 5) + (36 - 4) - 72) / 5;
+
+        System.out.println(result);
+    }
+}
