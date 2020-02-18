93620e285ee17cdd36cd7855ce089c449d51ff18
soneunsil <eunsil1023@gmail.com>
2020-02-18 17:49:39 +0900

    master-task/#25/while문과 do ~ while문의 활용_2

--- java/윤성우의 열혈 Java 프로그래밍/Chap5_4_2.java
+++ java/윤성우의 열혈 Java 프로그래밍/Chap5_4_2.java
@@ -4,17 +4,18 @@
  */
 public class Chap5_4_2 {
     public static void main(String[] args) {
-        int num = 1;
-        int sum = 100;
-
-        while (num <= 100) {
-            System.out.println(num);
-            num++;
-        }
+        int n = 0;
 
         do {
-            System.out.println(sum);
-            sum--;
-        } while (sum >= 1);
+            n++;
+            System.out.print(n + " ");
+        } while (n < 100);
+
+        System.out.println("");
+
+        while (n >= 1) {
+            System.out.print(n + " ");
+            n--;
+        }
     }
 }
