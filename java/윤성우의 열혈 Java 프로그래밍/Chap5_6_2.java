e65c3195533c5680af3af2e00bd5b91c780fb97a
soneunsil <eunsil1023@gmail.com>
2020-02-18 17:57:39 +0900

    master-task/#25/continue문과 break문의 활용_2

--- java/윤성우의 열혈 Java 프로그래밍/Chap5_6_2.java
+++ java/윤성우의 열혈 Java 프로그래밍/Chap5_6_2.java
@@ -1,21 +1,21 @@
 /*
-자연수 1부터 시작해서 모드느 홀수를 더해 나간다. 그리고 그 합이 언제(몇을 더했을 때) 1000을 넘어서는지,
+자연수 1부터 시작해서 모든 홀수를 더해 나간다. 그리고 그 합이 언제(몇을 더했을 때) 1000을 넘어서는지,
 그리고 1000을 넘어선 값은 얼마가 되는지 계산하여 출력하는 프로그램을 작성해보자.
  */
 public class Chap5_6_2 {
     public static void main(String[] args) {
-        int num = 1;
-        int sum = 0;
+        int odd = 1;
+        int result = 0;
 
         while (true) {
-            if ((num % 2) != 0) {
-                sum += num;
+            if ((odd % 2) != 0) {
+                result += odd;
             }
-            if (sum > 1000) {
-                System.out.println(num + "을 더했을 때 1000을 넘어섰음" + "\n" + "1000을 넘어선 값: " + sum);
+            if (result > 1000) {
+                System.out.println(odd + "을 더했을 때 1000을 넘어섰음" + "\n" + "1000을 넘어선 값: " + result);
                 break;
             }
-            num++;
+            odd++;
         }
     }
 }
